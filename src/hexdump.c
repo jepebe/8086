05a5cd7e89aae493d22de462debffc2c7ebf7121
Jean-Paul Balabanian <jepebe@users.noreply.github.com>
2022-02-06 15:25:29 +0100

    Improve hexdump
    Hexdump now has an additional column for ASCII output and handles
    files with non-power of 16 sizes.

--- src/hexdump.c
+++ src/hexdump.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 #include "io.h"
+#include "80x86.h"
+
 
 char *basename(char *path, char *default_basename) {
     char *name = strrchr(path, '/');
@@ -26,17 +28,40 @@ int main(int argc, char *argv[argc]) {
     Binary binary = read_binary(argv[1]);
     printf("Path : %s\n", binary.path);
     printf("Size : %lu bytes\n", binary.size);
-    
-    for (size_t i = 0; i < binary.size; ++i) {
-        if ((i % 16) == 0) {
-            printf("%04lx ", i);
+
+    size_t segments = binary.size / 16;
+    // add segment if size is not divisible by 16
+    segments += (binary.size % 16) > 0 ?  1 : 0;
+
+    for (size_t segment = 0; segment < segments; ++segment) {
+        printf("$%04lX  ", segment * 16);
+
+        size_t row_start = segment * 16;
+        size_t row_end = row_start + 16;
+        for (size_t row_index = row_start; row_index < row_end; ++row_index) {
+            if(row_index < binary.size) {
+                printf("%02X ", binary.data[row_index] & 0xFF);
+            } else {
+                printf("   ");
+            }
         }
 
-        printf("%02x ", binary.data[i] & 0xFF);
+        printf(" ");
 
-        if (((i + 1) % 16) == 0) {
-            printf("\n");
+        for (size_t row_index = row_start; row_index < row_end; ++row_index) {
+            if(row_index < binary.size) {
+                u8 c = binary.data[row_index];
+                if (c <= 0x20) {
+                    printf("\xe2\x90%c", c + 0x80);
+                } else {
+                    printf("%c", c);
+                }
+            } else {
+                printf(" ");
+            }
         }
+        printf("\n");
     }
+
     return 0;
 }
